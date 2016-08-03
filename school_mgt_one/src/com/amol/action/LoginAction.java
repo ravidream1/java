178d2217bed1bd5dda8070a8820dab38f6444626
Ravi Thapa <ravidream1@gmail.com>
2016-08-03 09:02:41 -0500

    An application to explain @WebServlet and Servlet configuration on web.xml.

--- school_mgt_one/src/com/amol/action/LoginAction.java
+++ school_mgt_one/src/com/amol/action/LoginAction.java
@@ -2,31 +2,39 @@ package com.amol.action;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.HashMap;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import com.amol.bean.Login;
-import com.amol.dao.impl.LoginDaoImpl;
-import com.amol.service.LoginService;
-import com.amol.service.impl.LoginServiceImpl;
 
 /**
  * Servlet implementation class LoginAction
  */
 
 
+/*
+ * <servlet>
+<servlet-name>loginAction</servlet-name> <!-- any name -->
+<servlet-class>com.amol.action.LoginAction</servlet-class> <!-- class name -->
+</servlet>
+
+<servlet-mapping>
+<servlet-name>loginAction</servlet-name> <!-- any name -->
+<url-pattern>LoginAction</url-pattern><!--  method name -->
+</servlet-mapping>
+
+
+In place of above configuration on web.xml we can just write @WebServlet("/LoginAction")
+*
+*/
+
+
+
 @WebServlet("/LoginAction")
 public class LoginAction extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
-	//LoginService loginService = null;
 
     /**
      * Default constructor. 
@@ -46,36 +54,8 @@ public class LoginAction extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		System.out.println("test");
-		
-		
-		String username = request.getParameter("username");
-		String password = request.getParameter("password");
-		
-		System.out.println("username:" + username);
-		System.out.println("password:" + password);
-		
-		Login login = new Login();
-		login.setUsername(username);
-		login.setPassword(password);
-		
-		
-		LoginDaoImpl ldi = new LoginDaoImpl();
-		boolean check = ldi.login(login);
-		//loginService = new LoginServiceImpl();
-		//boolean check = loginService.login(login);
-		
-		if(check){
-			
-			request.getSession().setAttribute("username", username);
+	
 
-			response.sendRedirect("home.jsp");
-
-			System.out.println("Login successfull.");
-		}
-		
-		
-     }
+	}
 
 }
